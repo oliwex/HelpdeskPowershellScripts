4c2b7ac6b3f8cbfbf1b49b47bf87e6caa67a0d67
oliwex <oliwexminio@gmail.com>
2019-07-14 21:05:36 +0200

    Adding translation from whatsmate api

--- randomChuckNorrisJokeInPolish.ps1
+++ randomChuckNorrisJokeInPolish.ps1
@@ -1,1 +1,19 @@
-﻿Invoke-RestMethod -Uri https://api.chucknorris.io/jokes/random | Select -ExpandProperty value
+﻿$chuckNorrisJoke=Invoke-RestMethod -Uri https://api.chucknorris.io/jokes/random | Select -ExpandProperty value
+
+# ClientId and ClientSecretCode
+$clientId = "FREE_TRIAL_ACCOUNT"
+$clientSecret = "PUBLIC_SECRET"
+# Parameters to translate
+$nativeJokeLanguage = "en"
+$resultLanguage = "pl"
+#creating json object$jsonObj = @{'fromLang'=$nativeJokeLanguage;
+             'toLang'=$resultLanguage;
+             'text'=$chuckNorrisJoke;}
+
+$response = Invoke-WebRequest -Uri 'http://api.whatsmate.net/v1/translation/translate' `
+                          -Method Post   `
+                          -Headers @{"X-WM-CLIENT-ID"=$clientId; "X-WM-CLIENT-SECRET"=$clientSecret;} `
+                          -Body (ConvertTo-Json $jsonObj)
+
+Write-host  "Translation: " $response.Content
+
