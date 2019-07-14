883d38bae48280819798bb67eafd1a1f819904cf
oliwex <oliwexminio@gmail.com>
2019-07-14 21:10:18 +0200

    Adding description to script

--- randomChuckNorrisJokeInPolish.ps1
+++ randomChuckNorrisJokeInPolish.ps1
@@ -10,10 +10,12 @@ $resultLanguage = "pl"
              'toLang'=$resultLanguage;
              'text'=$chuckNorrisJoke;}
 
+#sending response into website
 $response = Invoke-WebRequest -Uri 'http://api.whatsmate.net/v1/translation/translate' `
                           -Method Post   `
                           -Headers @{"X-WM-CLIENT-ID"=$clientId; "X-WM-CLIENT-SECRET"=$clientSecret;} `
                           -Body (ConvertTo-Json $jsonObj)
 
+#writing joke on screen
 Write-host  "Translation: " $response.Content
 
