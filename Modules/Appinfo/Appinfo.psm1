7ce786939e3a2d2aaf8bc93792be32d206d106fe
minio <oliwexminio@gmail.com>
2020-01-06 20:34:11 +0100

    Create module for Applocker.

---
+++ Modules/Appinfo/Appinfo.psm1
@@ -0,0 +1,23 @@
+﻿<#
+    .SYNOPSIS
+    Loads function into module
+    
+    .DESCRIPTION
+    This is autoloader for function in module
+    
+    .LIST OF APPLICATIONS
+    Get-ApplockerListOfApps
+
+    .LINK
+    Applocker
+    https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-defender-application-control/applocker/what-is-applocker
+#>
+
+$functionPath=$PSScriptRoot + "\Functions\"
+
+$functionList=Get-ChildItem -Path $functionPath -Name
+
+foreach($function in $functionList)
+{
+    . ($functionPath + $function)
+}
---
+++ Modules/Appinfo/Functions/Test-ApplockerWorking.ps1
@@ -0,0 +1,28 @@
+﻿function Test-ApplockerWorking
+{
+    <#
+    .SYNOPSIS
+    Test if Applocker work
+    
+    .DESCRIPTION
+    Function test if applocker works in operting system.
+    
+    .OUTPUTS
+    True or False
+    
+    .LINK
+    Get-Service
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-service?view=powershell-6
+
+    .LINK
+    Applocker
+    https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-defender-application-control/applocker/what-is-applocker
+
+    .EXAMPLE
+    Test-Applocker
+
+    #>
+    ((Get-Service *AppIDSvc*).Status -eq "Running")
+}
+
+Test-Applocker
