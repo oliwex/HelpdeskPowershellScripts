bba37227829dd4a6771aa75079200500b5df12c9
oliwex <oliwexminio@gmail.com>
2020-07-17 13:13:09 +0200

    Adding Remove-UserFunction
    Function which removes user from system. From filesystem and and system as registry. Function needs test

---
+++ Random/Remove-User.ps1
@@ -0,0 +1,13 @@
+﻿#Remove user from filesystem-Registry settings and files
+
+
+$user="testUser"
+Function Remove-UserProfile($userName)
+{
+    $userProfile=Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -eq $userName } | Remove-CimInstance
+    Remove-Item -Recurse -Force ($userProfile).LocalPath
+}
+
+Remove-UserProfile -userName $user
+
+
