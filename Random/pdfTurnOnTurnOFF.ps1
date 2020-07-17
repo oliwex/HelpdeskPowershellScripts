240f244c24bd0eb6167bcca7d4cc1e8ccc6a199d
oliwex <oliwexminio@gmail.com>
2020-07-17 14:00:39 +0200

    Turn On and Turn Off Report
    Turn On and Turn Off Report into PDF

---
+++ Random/pdfTurnOnTurnOFF.ps1
@@ -0,0 +1,13 @@
+﻿$element=Get-WinEvent -FilterHashTable @{ ProviderName = 'Microsoft-Windows-Power-TroubleShooter'  ; Id = 1 }|Select-Object -Property @{n='Sleep';e={Get-Date -Date $($_.Properties[0].Value) -Format "dd-MM-yyyy @ hh:mm:ss"}},@{n='Wake';e={Get-Date -Date $($_.Properties[1].Value) -Format "dd-MM-yyyy @ hh:mm:ss"}}
+
+$DataTable2 = @(
+    $element
+)
+
+New-PDF {
+
+    New-PDFText -Text 'Turn on and Turn off computer' -Font HELVETICA -FontColor RED
+
+    New-PDFTable -DataTable $DataTable2
+
+} -FilePath "C:\Users\lama\Desktop\powershell\Example06.pdf" -Show
