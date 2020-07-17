1acfdaafd03ff8bbd248b8b83f8d166e15c9a3fb
oliwex <oliwexminio@gmail.com>
2020-07-17 13:58:43 +0200

    Experiment
    Array vs list experiment

---
+++ Random/arrayVSlist.ps1
@@ -0,0 +1,5 @@
+﻿$range = 1..1000; 
+$IntList=[System.Collections.Generic.List[int]]::new(); 
+$IntArray = @(); 
+write-output "List method(ms): $((measure-command -expression { $range | %{ $IntList.Add($_)}}).TotalMilliseconds)" 
+write-output "Array method(ms): $((measure-command -expression {$range | % {$IntArray += $_}}).TotalMilliseconds)"
