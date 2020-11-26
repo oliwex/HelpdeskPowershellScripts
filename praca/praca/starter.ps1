d3fc50c3ed7752a967c94edd26791dfcf5ce39bd
Minio <oliwexminio@gmail.com>
2020-11-26 22:58:42 +0100

    Trzeba wykonać testy raportowania

--- praca/praca/starter.ps1
+++ praca/praca/starter.ps1
@@ -96,114 +96,6 @@ function Get-FilesReport
     return $filesReport
 }
 
-function New-ReportTitle
-{
-param(
-
-        [Parameter(Position = 0, Mandatory = $true)]
-        [String]$date,
-        [Parameter(Position = 1, Mandatory = $true)]
-        [String]$titleColor,
-        [Parameter(Position = 2, Mandatory = $true)]
-        [String]$subtitleColor,
-        [Parameter(Position = 3, Mandatory = $true)]
-        [String]$dataColor
-)
-    New-PDFText -Text "Raport monitorowania stacji roboczej" -Font HELVETICA -FontColor $titleColor -FontBold $true
-    New-PDFText -Text "Monitorowanie wykonano dnia: ",$date -Font HELVETICA -FontColor $subtitleColor,$dataColor
-}
-
-function New-ReportList
-{
-param(
-
-        [Parameter(Position = 0, Mandatory = $true)]
-        [String]$textColor,
-        [Parameter(Position = 1, Mandatory = $true, ValueFromPipeline = $true)]
-        $report
-)
-    New-PDFText -Text "Sprawdzono: " -Font HELVETICA -FontColor BLACK
-    New-PDFList -Indent 10 -Symbol bullet {
-           foreach($element in $report.Keys)
-           {
-                New-PDFListItem -Text $element
-           }
-        }
-}
-function New-Report1Level
-{
-param(
-
-        [Parameter(Position = 0, Mandatory = $true)]
-        [String]$reportBlock,
-        [Parameter(Position = 1, Mandatory = $true, ValueFromPipeline = $true)]
-        $report
-)
-    New-PDFText -Text $reportBlock -Font HELVETICA -FontColor BLUE
-    New-PDFTable -DataTable $($report.$reportBlock)
-}
-
-function New-Report2Level
-{
-param(
-
-        [Parameter(Position = 0, Mandatory = $true)]
-        [String]$reportBlock,
-        [Parameter(Position = 1, Mandatory = $true, ValueFromPipeline = $true)]
-        $report
-)
-    
-    New-PDFText -Text $reportBlock -Font HELVETICA -FontColor BLUE
-    foreach ($element in $report.$reportBlock.Keys)
-    {
-        New-PDFText -Text $element -Font HELVETICA -FontColor GREEN
-        New-PDFTable -DataTable $($report.$reportBlock.$element)
-    }
-
-}
-
-function Generate-Report
-{
-param(
-
-        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
-        $fullReport,
-        [Parameter(Position = 1, Mandatory = $true)]
-        $datetime,
-        [Parameter(Position = 2, Mandatory = $true)]
-        $path
-)
-    
-    New-PDF {
-        ##########TITLE##############
-        New-ReportTitle -date $datetime -titleColor BLUE -subtitleColor BLACK -dataColor GREEN
-        $fullReport | New-ReportList -textColor BLACK
-    
-        #########HARDWARE#############
-        $fullReport | New-Report2Level -reportBlock HARDWARE
-        #########PRINTER#############
-        $fullReport | New-Report2Level -reportBlock PRINTER
-        #########QUOTA#############
-        $fullReport | New-Report1Level -reportBlock QUOTA
-        #########FILESHARE#############
-        $fullReport | New-Report2Level -reportBlock FILESHARE
-        #########SOFTWARE#############
-        $fullReport | New-Report2Level -reportBlock SOFTWARE
-        #########DEFENDER#############
-        $fullReport | New-Report1Level -reportBlock DEFENDER
-        #########FIREWALL#############
-        $fullReport | New-Report2Level -reportBlock FIREWALL
-        #########LOG#############
-        $fullReport | New-Report2Level -reportBlock LOG
-        #########NETWORK#############
-        $fullReport | New-Report1Level -reportBlock NETWORK
-        #########SERVICE#############
-        $fullReport | New-Report2Level -reportBlock SERVICE
-
-    } -FilePath $path -Show
-}
-
-
 ###########################VARIABLES###################################
 $monitoredOU="KOMPUTERY"
 $computerList=(Get-ADComputer -Filter * -SearchBase "OU=$monitoredOU, DC=domena, DC=local").Name
@@ -215,7 +107,7 @@ $pathToSharedFolder="\\$env:COMPUTERNAME"
 $departmentName="DP"
 
 $pathToScript="C:\TEST\skrypt.ps1"
-$isScriptExist=Test-Path -Path $pathToScript -PathType Leaf
+$pathToReportGenerator="C:\TEST\reportGenerator.ps1"
 
 #######FOR DATA AQUISITION##########
 $softwareList = [ordered]@{
@@ -236,18 +128,7 @@ $filesReport=Get-FilesReport -userName $userName -groupName $groupName -pathToSh
 ##                                                                    ##
 ########################################################################
 ########################################################################
-#region PDFCreator
-$path=Read-Host -Prompt "Podaj ścieżkę do raportowania"
-$testPath=Test-Path -Path $path -PathType Container
-if (-not($testPath))
-{
-    New-Item -Path $path -ItemType Directory
-}
 
-$datetime=Get-Date -Format "HH.mm_MM.dd.yyyy"
-$fileName="HOST1-$datetime.pdf"
-$reportPath=Join-Path -Path $path -ChildPath $fileName
-#endregion PDFCreator
 
 #Current State
 $gpoLast=Get-ADOrganizationalUnit -Filter {name -eq $monitoredOU} | Select-Object -ExpandProperty distinguishedname | Get-GPInheritance | Select-Object -ExpandProperty gpolinks | ForEach-Object {Get-GPO -Guid $_.gpoid} | Select-Object ModificationTime
@@ -266,13 +147,15 @@ while($true)
     {
         "1=POLITYKA,2=NULL"
         $fullReport=Invoke-Command -ComputerName HOST1 -FilePath $pathToScript -ArgumentList $softwareList,$filesReport
-        $fullReport | Generate-Report -datetime $datetime -path $reportPath
+        "raportuj"
+        Invoke-Command  -FilePath $pathToReportGenerator -ArgumentList $fullReport
     }
     if (($isLastExist -eq $true) -and ($isCurrentExist -eq $false))
     {
         "1=NULL,2=POLITYKA"
         $fullReport=Invoke-Command -ComputerName HOST1 -FilePath $pathToScript -ArgumentList $softwareList,$filesReport
-    $fullReport | Generate-Report -datetime $datetime -path $reportPath
+        "raportuj"
+        Invoke-Command -FilePath $pathToReportGenerator -ArgumentList $fullReport
     }
     if (($testLastNull -eq $false) -and ($testCurrentNull -eq $false))
     {
@@ -284,7 +167,8 @@ while($true)
         {
             "POLITYKI ISTNIEJĄ I ZOSTAŁY WYKONANE ZMIANY"
             $fullReport=Invoke-Command -ComputerName HOST1 -FilePath $pathToScript -ArgumentList $softwareList,$filesReport
-            $fullReport | Generate-Report -datetime $datetime -path $reportPath
+            "raportuj"
+            Invoke-Command  -FilePath $pathToReportGenerator -ArgumentList $fullReport
         }
     }
     $gpoLast=$gpoCurrent 
