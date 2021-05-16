e565dc052b6083925614ed258bda155f8973c8a6
Minio <oliwexminio@gmail.com>
2021-05-16 22:35:22 +0200

    add script

---
+++ proba/Untitled1.ps1
@@ -0,0 +1,4 @@
+﻿$(Get-ADOrganizationalUnit -Filter *).DistinguishedName
+
+$OUpath = 'OU=WIOH,OU=DN,OU=UZYTKOWNICY,DC=domena,DC=local'
+Get-ADUser -Filter * -SearchBase $OUpath -Properties AccountExpirationDate,AccountNotDelegated,AllowReversiblePasswordEncryption,CannotChangePassword,CanonicalName,Certificates,City,CN,Company,Country,Created,Department,Description,DisplayName,DistinguishedName,Division,EmailAddress,EmployeeID,EmployeeNumber,Enabled,Fax,GivenName,HomeDirectory,HomeDirRequired,HomeDrive,HomePage,HomePhone,Initials,LastLogonDate,LogonWorkstations,Manager,MemberOf,
