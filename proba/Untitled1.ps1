9c5846922b7303418f0e2ad0298c5a35f260daba
Minio <oliwexminio@gmail.com>
2021-05-19 22:36:01 +0200

    creating function

--- proba/Untitled1.ps1
+++ proba/Untitled1.ps1
@@ -1,4 +1,22 @@
-﻿$(Get-ADOrganizationalUnit -Filter *).DistinguishedName
+﻿function Get-UserFromOU
+{
+Param(
+        [Parameter(Mandatory=$true)]
+        [alias("OU","OrganisationalUnit")]
+        [String] $OUpath,
+        [Switch] $extended
+    )
 
-$OUpath = 'OU=WIOH,OU=DN,OU=UZYTKOWNICY,DC=domena,DC=local'
-Get-ADUser -Filter * -SearchBase $OUpath -Properties AccountExpirationDate,AccountNotDelegated,AllowReversiblePasswordEncryption,CannotChangePassword,CanonicalName,Certificates,City,CN,Company,Country,Created,Department,Description,DisplayName,DistinguishedName,Division,EmailAddress,EmployeeID,EmployeeNumber,Enabled,Fax,GivenName,HomeDirectory,HomeDirRequired,HomeDrive,HomePage,HomePhone,Initials,LastLogonDate,LogonWorkstations,Manager,MemberOf,
+    $data=Get-ADUser -Filter * -SearchBase $OUpath -SearchScope Onelevel -Properties AccountExpirationDate,AccountNotDelegated,AllowReversiblePasswordEncryption,CannotChangePassword,CanonicalName,Certificates,City,CN,Company,Country,Created,Department,Description,DisplayName,DistinguishedName,Division,EmailAddress,EmployeeID,EmployeeNumber,Enabled,Fax,GivenName,HomeDirectory,HomeDirRequired,HomeDrive,HomePage,HomePhone,Initials,LastLogonDate,LogonWorkstations,Manager,MemberOf,MobilePhone,Modified,Name,ObjectCategory,ObjectClass,ObjectGuid,Office,OfficePhone,Organization,OtherName,PasswordExpired,PasswordLastSet,PasswordNeverExpires,PasswordNotRequired,POBox,PostalCode,PrimaryGroup,ProfilePath,ProtectedFromAccidentalDeletion,SamAccountName,ScriptPath,ServicePrincipalName,SID,SIDHistory,SmartCardLogonRequired,State,StreetAddress,Surname,Title,TrustedForDelegation,TrustedToAuthForDelegation,UseDESKeyOnly,UserPrincipalName #,msDS-SupportedEncryptionTypes,User-Account-Control
+
+    if ($extended)
+    {
+        $data
+    }
+    else
+    {
+        $data | Select DistinguishedName,GivenName,Name,ObjectClass,ObjectGuid,SamAccountName,SID,Surname,UserPrincipalName,CannotChangePassword,PasswordNeverExpires,AllowReversiblePasswordEncryption,Enabled,SmartCardLogonRequired,TrustedForDelegation,UseDESKeyOnly #,msDS-SupportedEncryptionTypes,User-Account-Control
+    }
+}
+
+Get-UserFromOU -OUpath "OU=BDG,OU=UZYTKOWNICY,DC=domena,DC=local" -extended:$false
