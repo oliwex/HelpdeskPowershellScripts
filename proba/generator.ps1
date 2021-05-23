3634919c15fd73d3909c82538ffe3e37cfe9ddb7
Minio <oliwexminio@gmail.com>
2021-05-23 22:43:26 +0200

    User report creating
    Needs few improvements

--- proba/generator.ps1
+++ proba/generator.ps1
@@ -1,8 +1,53 @@
-﻿Documentimo -FilePath "C:\Starter-AD.docx" {
+﻿function ConvertTo-Hashtable
+{
+Param(
+        [Parameter(Mandatory=$true)]
+        [alias("PsObject")]
+        $Object
+    )
+    $hashtable=@{}
+    $Object.psobject.properties | Foreach { $hashtable[$_.Name] = $_.Value }
+    $hashtable
+}
+(Get-ADOrganizationalUnit -Filter *).DistinguishedName
+########################################################
+########################################################
+########################################################
+########################################################
+########################################################
+
+function Get-UserFromOU
+{
+Param(
+        [Parameter(Mandatory=$true)]
+        [alias("OU","OrganisationalUnit")]
+        [String] $OUpath,
+        [Switch] $extended
+    )
+
+    $data=Get-ADUser -Filter * -SearchBase $OUpath -SearchScope Onelevel -Properties *
+
+    if ($extended)
+    {
+        $data
+    }
+    else
+    {
+        $data | Select DistinguishedName,GivenName,Name,ObjectClass,ObjectGuid,SamAccountName,SID,Surname,UserPrincipalName,CannotChangePassword,PasswordNeverExpires,AllowReversiblePasswordEncryption,Enabled,SmartCardLogonRequired,TrustedForDelegation,UseDESKeyOnly,msDS-SupportedEncryptionTypes,userAccountControl
+    }
+}
+
+########################################################
+########################################################
+########################################################
+########################################################
+########################################################
+
+Documentimo -FilePath "C:\reporty\Starter-AD.docx" {
     DocTOC -Title 'Table of content'
 
     DocPageBreak
-
+    
     DocText {
         "Jest to dokumentacja domeny ActiveDirectory przeprowadzona w domena.local. Wszytskie informacje są tajne"
     }
@@ -13,6 +58,7 @@
         DocText {
             "Ta część zawiera spis jednostek organizacyjnych wraz z informacjami o każdej z nich"
         }
+        
         #DocTable -DataTable $ADForest.ForestInformation -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle 'Forest Summary'
         DocText -LineBreak
     }
@@ -33,7 +79,18 @@
         DocText {
             "Ta część zawiera spis użytkowników w każdej jednostce organizacyjnej"
         }
-        #DocTable -DataTable $ADForest.ForestInformation -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle 'Forest Summary'
+
+        $OUs=(Get-ADOrganizationalUnit -Filter *).DistinguishedName
+        foreach($OU in $OUs)
+        {
+            $UserInfo=(Get-UserFromOU -OUpath $OU -extended:$false)
+            foreach($User in $UserInfo)
+            {
+                $hashtable=ConvertTo-Hashtable -Object $User
+                DocTable -DataTable $hashtable -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle 'User'
+            }
+            DocText -LineBreak 
+        }
         DocText -LineBreak
     }
 
@@ -48,5 +105,4 @@
         DocText -LineBreak
     }
     #>
-
 }
