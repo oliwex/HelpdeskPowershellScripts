e151b4bc589702cba8525aa7d2508a53ec9f46d4
Minio <oliwexminio@gmail.com>
2021-05-30 22:49:51 +0200

    Code dividing

--- proba/generator.ps1
+++ proba/generator.ps1
@@ -49,7 +49,7 @@ Param(
         [Switch] $isExtended
     )
 
-    $data=Get-ADOrganizationalUnit -Filter * -Properties * -SearchBase $oupath
+    $data=Get-ADOrganizationalUnit -Filter * -Properties * -SearchBase $oupath -SearchScope 0
 
     if ($isExtended)
     {
@@ -60,7 +60,70 @@ Param(
         $data | Select Name,Description,Street,City,State,PostalCode,Country,ManagedBy
     }
 }
-
+function Get-GPOPolicy {
+    [CmdletBinding()]
+    param(
+        [Array] $GroupPolicies,
+        [string] $Domain = $Env:USERDNSDOMAIN,
+        [string] $Splitter
+    )
+    if ($null -eq $GroupPolicies) {
+        $GroupPolicies = Get-GPO -Domain $Domain -All
+    }
+    ForEach ($GPO in $GroupPolicies) {
+        [xml]$XmlGPReport = $GPO.generatereport('xml')
+        #GPO version
+        if ($XmlGPReport.GPO.Computer.VersionDirectory -eq 0 -and $XmlGPReport.GPO.Computer.VersionSysvol -eq 0) {
+            $ComputerSettings = "NeverModified"
+        } else {
+            $ComputerSettings = "Modified"
+        }
+        if ($XmlGPReport.GPO.User.VersionDirectory -eq 0 -and $XmlGPReport.GPO.User.VersionSysvol -eq 0) {
+            $UserSettings = "NeverModified"
+        } else {
+            $UserSettings = "Modified"
+        }
+        #GPO content
+        if ($null -eq $XmlGPReport.GPO.User.ExtensionData) {
+            $UserSettingsConfigured = $false
+        } else {
+            $UserSettingsConfigured = $true
+        }
+        if ($null -eq $XmlGPReport.GPO.Computer.ExtensionData) {
+            $ComputerSettingsConfigured = $false
+        } else {
+            $ComputerSettingsConfigured = $true
+        }
+        #Output
+        [PsCustomObject] @{
+            'Name'                   = $XmlGPReport.GPO.Name
+            'Links'                  = $XmlGPReport.GPO.LinksTo | Select-Object -ExpandProperty SOMPath
+            'Has Computer Settings'  = $ComputerSettingsConfigured
+            'Has User Settings'      = $UserSettingsConfigured
+            'User Enabled'           = $XmlGPReport.GPO.User.Enabled
+            'Computer Enabled'       = $XmlGPReport.GPO.Computer.Enabled
+            'Computer Settings'      = $ComputerSettings
+            'User Settings'          = $UserSettings
+            'Gpo Status'             = $GPO.GpoStatus
+            'Creation Time'          = $GPO.CreationTime
+            'Modification Time'      = $GPO.ModificationTime
+            'WMI Filter'             = $GPO.WmiFilter.name
+            'WMI Filter Description' = $GPO.WmiFilter.Description
+            'Path'                   = $GPO.Path
+            'GUID'                   = $GPO.Id
+            'ACLs'                   = $XmlGPReport.GPO.SecurityDescriptor.Permissions.TrusteePermissions | ForEach-Object -Process {
+                New-Object -TypeName PSObject -Property @{
+                    'User'            = $_.trustee.name.'#Text'
+                    'Permission Type' = $_.type.PermissionType
+                    'Inherited'       = $_.Inherited
+                    'Permissions'     = $_.Standard.GPOGroupedAccessEnum
+                }
+            }
+        }
+    }
+}
+#$lama=Get-GPOPolicy
+#$lama | Where-Object {$_.Links -contains "domena.local/KOMPUTERY"}
 
 ########################################################
 ########################################################
@@ -84,10 +147,23 @@ Documentimo -FilePath "C:\reporty\Starter-AD.docx" {
             "Ta część zawiera spis jednostek organizacyjnych wraz z informacjami o każdej z nich"
         }
         
-        #DocTable -DataTable $ADForest.ForestInformation -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle 'Forest Summary'
+        $ous=(Get-ADOrganizationalUnit -Filter "*").DistinguishedName
+        
+        foreach($ou in $ous)
+        {
+            DocNumbering -Text $ou -Level 1 -Type Numbered -Heading Heading1 {
+            
+            $ouInfo=Get-OUsInformation -OU $ou -Extended:$false
+
+            DocTable -DataTable $ouInfo -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle $ou -Transpose
+            
+            DocText -LineBreak
+            }
+        }
+       
         DocText -LineBreak
     }
-    
+    <#
     #Grupy
     DocNumbering -Text 'Spis grup' -Level 0 -Type Numbered -Heading Heading1 {
         
@@ -97,7 +173,8 @@ Documentimo -FilePath "C:\reporty\Starter-AD.docx" {
         #DocTable -DataTable $ADForest.ForestInformation -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle 'Forest Summary'
         DocText -LineBreak
     }
-
+    
+    
     #Użytkownicy
     DocNumbering -Text 'Spis użytkowników' -Level 0 -Type Numbered -Heading Heading1 {
         
@@ -111,24 +188,19 @@ Documentimo -FilePath "C:\reporty\Starter-AD.docx" {
         {
             DocNumbering -Text $uo -Level 1 -Type Numbered -Heading Heading1 {
             
-            $ouInfo=Get-OUsInformation -OU $ou -Extended:$false
-
-            DocTable -DataTable $ouInfo -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle $($ouInfo.Name.ToString()) -Transpose
-            DocText -LineBreak
-
-            $usersInfo=(Get-UsersFromOU -OUpath $OU -Extended:$false)
-            foreach($user in $usersInfo)
-            {
-                DocTable -DataTable $user -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle $user.Name -Transpose
-                DocText -LineBreak
-                #po userze
-            }
+            
+                $usersInfo=(Get-UsersFromOU -OUpath $ou -Extended:$false)
+                foreach($user in $usersInfo)
+                {
+                    DocTable -DataTable $user -Design ColorfulGridAccent5 -AutoFit Window -OverwriteTitle $user.Name -Transpose
+                    DocText -LineBreak
+                    #po userze
+                }
             }
         }
         DocText -LineBreak
     }
 
-    <#
     #Inna część
     DocNumbering -Text 'Spis użytkowników' -Level 0 -Type Numbered -Heading Heading1 {
         
