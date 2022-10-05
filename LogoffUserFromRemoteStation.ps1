fb0b76e275883b09bb7bb59d1c60481275bb16fa
Minio <oliwexminio@gmail.com>
2022-10-05 20:30:45 +0200

    Repairing Logoff from remote station and QuserIntoPsCustomObject

--- LogoffUserFromRemoteStation.ps1
+++ LogoffUserFromRemoteStation.ps1
@@ -1,3 +1,81 @@
-﻿
-#Get session id of local user logged in by: quser /server:ComputerName
-Invoke-Command -ComputerName ComputerName -ScriptBlock { logoff 5 } # logoff user by session id from quser /server:ComputerName
+﻿function Check-LogonSessions
+{
+[CmdletBinding()]
+    param (
+        [Parameter(HelpMessage = "ComputerName to check logon sessions",Position=0,ValueFromPipeline)]
+        [Alias("Computers")]
+        [string]$computerName
+    )
+    begin
+    {}
+    process
+    {
+        $userFromStationCSV=(quser  /server:$computerName 2>&1) -split "\n" -replace '\s{2,}', ','
+        if ($userFromStationCSV -match "ID")
+        {
+            $userFromStationPSCustomObjects=($userFromStationCSV | convertfrom-csv -Delimiter ',')
+            foreach ($userFromStationPSCustomObject in $userFromStationPSCustomObjects)
+            {
+                if ($userFromStationPSCustomObject.STATE -like "Active") 
+                {
+                    [PScustomObject]@{
+                        COMPUTERNAME=$computerName
+                        USERNAME=$userFromStationPSCustomObject.USERNAME
+                        SESSIONNAME=$userFromStationPSCustomObject.SESSIONNAME
+                        ID=$userFromStationPSCustomObject.ID
+                        STATE=$userFromStationPSCustomObject.STATE
+                        IDLE_TIME=$userFromStationPSCustomObject."IDLE TIME"
+                        LOGON_TIME=$userFromStationPSCustomObject."LOGON TIME"
+                    }
+                }
+                else
+                {
+                    [PScustomObject]@{
+                        COMPUTERNAME=$computerName
+                        USERNAME=$userFromStationPSCustomObject.USERNAME
+                        SESSIONNAME="NOT AVAILABLE"
+                        ID=$userFromStationPSCustomObject.SESSIONNAME
+                        STATE=$userFromStationPSCustomObject.ID
+                        IDLE_TIME=$userFromStationPSCustomObject.STATE
+                        LOGON_TIME=$userFromStationPSCustomObject."IDLE TIME"
+                    }
+                }
+            }
+        }
+        else
+        {
+        "Blad, lub zaden uzytkownik nie jest zalogowany"
+        }
+    }
+    end
+    {}
+}
+
+function Remove-LogonSessions
+{
+[CmdletBinding()]
+    param (
+        [Parameter(HelpMessage = "ComputerName to kill stuck sessions",Position=0,ValueFromPipeline)]
+        [Alias("Computers")]
+        [string]$computerName
+    )
+    begin
+    {}
+    process
+    {
+        $ids=(Check-LogonSessions -Computers $computerName | Where-Object {$_.State -eq "Disc"}).ID
+        foreach($id in $ids)
+        {
+            logoff $id /server:$computerName
+        }
+    }
+    end
+    {}
+}
+
+$path=Read-Host "Podaj ścieżkę do listy stacji: "
+
+
+Get-Content -Path $path | Remove-LogonSessions
+
+Get-Content -Path $path | Check-LogonSessions
--- QuserIntoPSCustomObject.ps1
+++ QuserIntoPSCustomObject.ps1
@@ -1,5 +1,4 @@
-﻿#parse quser command into powershell pscustomobject
-function Check-LogonSessions
+﻿function Check-LogonSessions
 {
 [CmdletBinding()]
     param (
@@ -11,30 +10,34 @@ function Check-LogonSessions
     {}
     process
     {
-        $lama=(quser  /server:$computerName 2>&1) -split "\n" -replace '\s{2,}', ','
-        if ($lama -match "ID")
+        $userFromStationCSV=(quser  /server:$computerName 2>&1) -split "\n" -replace '\s{2,}', ','
+        if ($userFromStationCSV -match "ID")
         {
-            $lama | convertfrom-csv -Delimiter ','| ForEach-Object {
-                if ($($_).STATE -like "Active") 
+            $userFromStationPSCustomObjects=($userFromStationCSV | convertfrom-csv -Delimiter ',')
+            foreach ($userFromStationPSCustomObject in $userFromStationPSCustomObjects)
+            {
+                if ($userFromStationPSCustomObject.STATE -like "Active") 
                 {
                     [PScustomObject]@{
-                        USERNAME=$($_).USERNAME
-                        SESSIONNAME=$($_).SESSIONNAME
-                        ID=$($_).ID
-                        STATE=$($_).STATE
-                        IDLE_TIME=$($_)."IDLE TIME"
-                        LOGON_TIME=$($_)."LOGON TIME"
+                        COMPUTERNAME=$computerName
+                        USERNAME=$userFromStationPSCustomObject.USERNAME
+                        SESSIONNAME=$userFromStationPSCustomObject.SESSIONNAME
+                        ID=$userFromStationPSCustomObject.ID
+                        STATE=$userFromStationPSCustomObject.STATE
+                        IDLE_TIME=$userFromStationPSCustomObject."IDLE TIME"
+                        LOGON_TIME=$userFromStationPSCustomObject."LOGON TIME"
                     }
                 }
                 else
                 {
                     [PScustomObject]@{
-                        USERNAME=$($_).USERNAME
+                        COMPUTERNAME=$computerName
+                        USERNAME=$userFromStationPSCustomObject.USERNAME
                         SESSIONNAME="NOT AVAILABLE"
-                        ID=$($_).SESSIONNAME
-                        STATE=$($_).ID
-                        IDLE_TIME=$($_).STATE
-                        LOGON_TIME=$($_)."IDLE TIME"
+                        ID=$userFromStationPSCustomObject.SESSIONNAME
+                        STATE=$userFromStationPSCustomObject.ID
+                        IDLE_TIME=$userFromStationPSCustomObject.STATE
+                        LOGON_TIME=$userFromStationPSCustomObject."IDLE TIME"
                     }
                 }
             }
@@ -47,5 +50,3 @@ function Check-LogonSessions
     end
     {}
 }
-
-Check-LogonSessions -Computers ComputerName
